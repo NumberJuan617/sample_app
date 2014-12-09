c4668eb296489bc72eb0ad5cc1d2d22ee8464b65
NumberJuan617 <juanluisandujar10@gmail.com>
2014-12-09 09:11:03 -0500

    Adding user func

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,7 @@ class User < ActiveRecord::Base
 							 uniqueness: { case_sensitive: false }
 
 	has_secure_password	#allows for saving securely hashed passwords
-	validates :password, length: { minimum: 6 }
-
+  validates :password, length: { minimum: 6 }, allow_blank: true
 	# Returns the hash digest of the given string.
   	def User.digest(string)
 	    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
